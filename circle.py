2e8a4c176407bfe4b6e8bff185108380fcb0796a
matt venn <matt@mattvenn.net>
2014-07-09 08:26:41 +0100

    started layout code

--- circle.py
+++ circle.py
@@ -3,7 +3,7 @@ from math import pi
 
 speed(0)
 
-def circle(x=0,y=0,r=10,segments=40):
+def circle(x=0,y=0,r=10,segments=30):
     c = 2 * pi * r
     l = c / segments
     a = 360 / segments
@@ -15,18 +15,39 @@ def circle(x=0,y=0,r=10,segments=40):
         forward(l)
         left(a)
 
-def cross():
-    backward(10)
-    forward(20)
-    backward(10)
-    left(90)
-    backward(10)
-    forward(20)
+fh = open('results.csv')
+lines = fh.readlines()
 
-cross()
-circle(r=100)
-circle(r=100, x=100,y=0)
-circle(r=100, x=200,y=0)
+max_width = 300
+row_height = 100
+x = 0
+y = 0
+penup()
+goto(-300,-300)
+setx(0)
+sety(0)
+max_r = 0
+min_size = 10
+for line in lines:
+	num, size = line.split(',')
+	num = int(num)
+	size = int(size)
+	size += min_size
+	size = size * 0.5
+	if size > row_height:
+		print("circle too big, limiting")
+		size = row_height
+	if size > max_r:
+		max_r = size
+	print("drawing circle %d, size %d" % (num,size))
+	circle(r=size, x=x,y=y)
+	x += size * 2
+	if x > max_width:
+		x = 0
+		row_height = max_r * 2
+		max_r = 0
+		y += row_height
+getscreen().getcanvas().postscript(file="circles.eps")
 done()
 
     
