3cb2a81e85a39862b2306b42fcda42e1ad165ba7
matt venn <matt@mattvenn.net>
2015-04-13 13:28:09 +0100

    remove y placement

--- circle.py
+++ circle.py
@@ -1,82 +1,77 @@
-from turtle import *
+import turtle
 from math import pi
 
-#as fast as we can
-speed(0)
+# as fast as we can
+turtle.speed(0)
 
-#here's our circle function
-def circle(x=0,y=0,r=10,segments=30):
-    #work out the amount to go fowards and the amount to turn
+
+# here's our circle function
+def circle(x=0, y=0, r=10, segments=10):
+    # work out the amount to go fowards and the amount to turn
     c = 2 * pi * r
     l = c / segments
     a = 360 / segments
 
-    #move to the bottom of the circle
-    penup()
-    goto(x-l/2,y-r)
-    pendown()
+    # move to the bottom of the circle
+    turtle.penup()
+    turtle.goto(x-l/2, y-r)
+    turtle.pendown()
 
-    #draw the circle
+    # draw the circle
     for s in range(segments):
-        forward(l)
-        left(a)
+        turtle.forward(l)
+        turtle.left(a)
 
-#hide the turtle so it doesn't get printed or laser cut
-hideturtle()
+# hide the turtle so it doesn't get printed or laser cut
+turtle.hideturtle()
 
-#open the file and read each line into a list called lines
+# open the file and read each line into a list called lines
 fh = open('results.csv')
 lines = fh.readlines()
 
-#some variables we'll need
-max_width = 300
-row_height = 100
-x = 0
-y = 0
-laser_spacing = 5
-max_r = 0
-min_size = 15
+# some settings:
+
+# scaling is how we scale the numbers in the file to size of circles
+# make this number bigger or smaller till all your beads fit on the screen
+scaling = 0.007
+x = -350  # start off on the left
+laser_spacing = 5  # space to leave between beads
+min_size = 10  # smallest bead
+max_size = 50  # largest bead
+thread_r = 2  # hole for thread
+
+# some variables we'll need
 last_size = 0
-thread_r = 2
+y = 0
 
 for line in lines:
-    #split the csv into 2 parts
+    # split the csv into 2 parts
     num, size = line.split(',')
-    #and turn them both from strings to ints
+    # and turn them both from strings to ints
     num = int(num)
     size = int(size)
-   
-    #scale the size to something small enough to cut
-    size = size * 0.01
 
-    #if the size is too small, we still need a bead, so add a bit on
+    # scale the size to something small enough to cut
+    size = size * scaling
+
+    # if the size is too small, we still need a bead, so add a bit on
     size += min_size
 
-    #stop any circle from being too big
-    if size > row_height:
+    # stop any circle from being too big
+    if size > max_size:
         print("circle too big, limiting")
-        size = row_height
-    #keep a track of the max size
-    if size > max_r:
-        max_r = size
+        size = max_size
 
-    #draw the circle
-    print("drawing circle %d, size %d" % (num,size))
+    # draw the circle
+    print("drawing circle %d, size %d" % (num, size))
     x = x + size + last_size + laser_spacing
-    #for the outer circle
-    circle(r=size, x=x,y=y)
-    #for the thread hole
-    circle(r=thread_r, x=x,y=y)
+    # for the outer circle
+    circle(r=size, x=x, y=y)
+    # for the thread hole
+    circle(r=thread_r, x=x, y=y)
     last_size = size
 
-    #this part is for the arrangement of the circles on the page
-    if x > max_width:
-        x = 0
-        row_height = max_r * 2
-        max_r = 0
-        y += row_height + laser_spacing
-
-#this line exports the canvas to an eps file we can laser cut
-getscreen().getcanvas().postscript(file="circles.eps")
+# this line exports the canvas to an eps file we can laser cut
+turtle.getscreen().getcanvas().postscript(file="circles.eps")
 print("done")
-done()
+turtle.done()
